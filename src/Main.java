7b48d6c0276cb2158e00f92498b59cd972f08373
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-05 22:54:29 +0530

    Feature 1: The main() method makes a call to the Analysis class to get the matches played in each season.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 import models.Delivery;
 import models.Match;
+import services.Analysis;
 import utils.CsvReader;
 
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Match> matches = CsvReader.loadMatches();
         List<Delivery> deliveries = CsvReader.loadDeliveries();
+
+        // Feature 1 - Matches per season
+        System.out.println("========================================");
+        Map<Integer, Integer> matchesPerSeason = Analysis.matchesPerSeason(matches);
+        for (Map.Entry<Integer, Integer> entry: matchesPerSeason.entrySet()) {
+            System.out.println("Season " + entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println("========================================");
 
 
     }
