8c73617f80c81b3b0209bacd4df66cca9c17964d
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-05 21:33:34 +0530

    Added the implementation logic for the first feature: The total matches per season. It returns a map, where the key is season and the value is the total matches played in that season.

--- src/services/Analysis.java
+++ src/services/Analysis.java
@@ -1,4 +1,21 @@
 package services;
 
+import models.Match;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Analysis {
+
+    public static Map<Integer, Integer> matchesPerSeason(List<Match> matches) {
+        Map<Integer, Integer> seasonMatchCount = new TreeMap<>();
+        for (Match match: matches) {
+            int season = match.getSeason();
+            seasonMatchCount.put(season, seasonMatchCount.getOrDefault(season, 0) + 1);
+        }
+        return seasonMatchCount;
+    }
+
+
 }
