3b70945460f6f37033f0efa6146f4e47cd28a496
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-05 21:05:43 +0530

    Added attributes, all arguments constructor, getters and setters in Delivery class

--- src/models/Delivery.java
+++ src/models/Delivery.java
@@ -1,4 +1,221 @@
 package models;
 
 public class Delivery {
+
+    private int matchId;
+    private int inning;
+    private String battingTeam;
+    private String bowlingTeam;
+    private int over;
+    private int ball;
+    private String batter;
+    private String nonStriker;
+    private String bowler;
+    private int isSuperOver;
+    private int wideRuns;
+    private int byeRuns;
+    private int legByeRuns;
+    private int noBallRuns;
+    private int penaltyRuns;
+    private int batterRuns;
+    private int extraRuns;
+    private int totalRuns;
+    private String playerDismissed;
+    private String dismissalKind;
+    private String fielder;
+
+    public Delivery(int matchId, int inning, String battingTeam, String bowlingTeam, int over, int ball,
+                    String batter, String nonStriker, String bowler, int isSuperOver, int wideRuns, int byeRuns,
+                    int legByeRuns, int noBallRuns, int penaltyRuns, int batterRuns, int extraRuns, int totalRuns,
+                    String playerDismissed, String dismissalKind, String fielder) {
+        this.matchId = matchId;
+        this.inning = inning;
+        this.battingTeam = battingTeam;
+        this.bowlingTeam = bowlingTeam;
+        this.over = over;
+        this.ball = ball;
+        this.batter = batter;
+        this.nonStriker = nonStriker;
+        this.bowler = bowler;
+        this.isSuperOver = isSuperOver;
+        this.wideRuns = wideRuns;
+        this.byeRuns = byeRuns;
+        this.legByeRuns = legByeRuns;
+        this.noBallRuns = noBallRuns;
+        this.penaltyRuns = penaltyRuns;
+        this.batterRuns = batterRuns;
+        this.extraRuns = extraRuns;
+        this.totalRuns = totalRuns;
+        this.playerDismissed = playerDismissed;
+        this.dismissalKind = dismissalKind;
+        this.fielder = fielder;
+    }
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getInning() {
+        return inning;
+    }
+
+    public void setInning(int inning) {
+        this.inning = inning;
+    }
+
+    public String getBattingTeam() {
+        return battingTeam;
+    }
+
+    public void setBattingTeam(String battingTeam) {
+        this.battingTeam = battingTeam;
+    }
+
+    public String getBowlingTeam() {
+        return bowlingTeam;
+    }
+
+    public void setBowlingTeam(String bowlingTeam) {
+        this.bowlingTeam = bowlingTeam;
+    }
+
+    public int getOver() {
+        return over;
+    }
+
+    public void setOver(int over) {
+        this.over = over;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public String getBatter() {
+        return batter;
+    }
+
+    public void setBatter(String batter) {
+        this.batter = batter;
+    }
+
+    public String getNonStriker() {
+        return nonStriker;
+    }
+
+    public void setNonStriker(String nonStriker) {
+        this.nonStriker = nonStriker;
+    }
+
+    public String getBowler() {
+        return bowler;
+    }
+
+    public void setBowler(String bowler) {
+        this.bowler = bowler;
+    }
+
+    public int getIsSuperOver() {
+        return isSuperOver;
+    }
+
+    public void setIsSuperOver(int isSuperOver) {
+        this.isSuperOver = isSuperOver;
+    }
+
+    public int getWideRuns() {
+        return wideRuns;
+    }
+
+    public void setWideRuns(int wideRuns) {
+        this.wideRuns = wideRuns;
+    }
+
+    public int getByeRuns() {
+        return byeRuns;
+    }
+
+    public void setByeRuns(int byeRuns) {
+        this.byeRuns = byeRuns;
+    }
+
+    public int getLegByeRuns() {
+        return legByeRuns;
+    }
+
+    public void setLegByeRuns(int legByeRuns) {
+        this.legByeRuns = legByeRuns;
+    }
+
+    public int getNoBallRuns() {
+        return noBallRuns;
+    }
+
+    public void setNoBallRuns(int noBallRuns) {
+        this.noBallRuns = noBallRuns;
+    }
+
+    public int getPenaltyRuns() {
+        return penaltyRuns;
+    }
+
+    public void setPenaltyRuns(int penaltyRuns) {
+        this.penaltyRuns = penaltyRuns;
+    }
+
+    public int getBatterRuns() {
+        return batterRuns;
+    }
+
+    public void setBatterRuns(int batterRuns) {
+        this.batterRuns = batterRuns;
+    }
+
+    public int getExtraRuns() {
+        return extraRuns;
+    }
+
+    public void setExtraRuns(int extraRuns) {
+        this.extraRuns = extraRuns;
+    }
+
+    public int getTotalRuns() {
+        return totalRuns;
+    }
+
+    public void setTotalRuns(int totalRuns) {
+        this.totalRuns = totalRuns;
+    }
+
+    public String getPlayerDismissed() {
+        return playerDismissed;
+    }
+
+    public void setPlayerDismissed(String playerDismissed) {
+        this.playerDismissed = playerDismissed;
+    }
+
+    public String getDismissalKind() {
+        return dismissalKind;
+    }
+
+    public void setDismissalKind(String dismissalKind) {
+        this.dismissalKind = dismissalKind;
+    }
+
+    public String getFielder() {
+        return fielder;
+    }
+
+    public void setFielder(String fielder) {
+        this.fielder = fielder;
+    }
 }
