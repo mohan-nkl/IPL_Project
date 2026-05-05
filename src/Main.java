60695534c4354799d93010bc6f3b8e59029c53ed
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-05 23:06:05 +0530

    Feature 3: The main() method makes a call to the Analysis class to get the total extra runs conceded by each team in 2016.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import models.Match;
 import services.Analysis;
 import utils.CsvReader;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,6 +33,27 @@ public class Main {
         }
         System.out.println("========================================");
 
+        // Feature 3: Total extra runs conceded by each team in 2016
+        System.out.println("========================================");
+        System.out.println("Total extra runs conceded by each team in 2016");
+        Set<Integer> matchIds = getMatchIds(matches, 2016);
+        Map<String, Integer> extraRunsByEachTeam = Analysis.totalExtraRunsConcededByEachTeam(matchIds, deliveries);
+        for (Map.Entry<String, Integer> entry: extraRunsByEachTeam.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println("========================================");
+
+
+    }
+
+    private static Set<Integer> getMatchIds(List<Match> matches, int season) {
+        Set<Integer> matchIds = new HashSet<>();
+        for (Match match: matches) {
+            if (match.getSeason() == season) {
+                matchIds.add(match.getMatchId());
+            }
+        }
+        return matchIds;
     }
 }
 
