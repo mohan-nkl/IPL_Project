4fa4f73c9409baf07b7cade593c6d9d7fe7234de
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-05 23:11:55 +0530

    Feature 4: The main() method makes a call to the Analysis class to get the best economic bowleres in 2015.

--- src/Main.java
+++ src/Main.java
@@ -36,10 +36,23 @@ public class Main {
         // Feature 3: Total extra runs conceded by each team in 2016
         System.out.println("========================================");
         System.out.println("Total extra runs conceded by each team in 2016");
-        Set<Integer> matchIds = getMatchIds(matches, 2016);
-        Map<String, Integer> extraRunsByEachTeam = Analysis.totalExtraRunsConcededByEachTeam(matchIds, deliveries);
+        Set<Integer> matchIds2016 = getMatchIds(matches, 2016);
+        Map<String, Integer> extraRunsByEachTeam = Analysis.totalExtraRunsConcededByEachTeam(matchIds2016, deliveries);
         for (Map.Entry<String, Integer> entry: extraRunsByEachTeam.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println("========================================");
+
+        // Feature 4: Best economic bowlers in 2015
+        System.out.println("========================================");
+        System.out.println("10 Best economic bowlers in 2015");
+        Set<Integer> matchIds2015 = getMatchIds(matches, 2015);
+        List<Map.Entry<String, Double>> bestBowlers = Analysis.getBestEconomicBowlers(matchIds2015, deliveries);
+        for (int i = 0; i < 10; i++) {
+            Map.Entry<String, Double> entry = bestBowlers.get(i);
+            System.out.print(entry.getKey() + " : ");
+            System.out.printf("%.2f", entry.getValue());
+            System.out.println();
         }
         System.out.println("========================================");
 
