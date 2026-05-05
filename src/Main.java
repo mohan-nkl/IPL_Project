7804554372f68be7357098008777fc12ef272782
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-05 22:50:16 +0530

    In the main class, loaded a list of matches and deliveries.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import models.Delivery;
+import models.Match;
+import utils.CsvReader;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<Match> matches = CsvReader.loadMatches();
+        List<Delivery> deliveries = CsvReader.loadDeliveries();
 
 
     }
