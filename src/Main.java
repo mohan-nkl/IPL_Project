4420533a1bfd89a436a9068a61f455d110e75526
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-05 22:58:37 +0530

    Feature 2: The main() method makes a call to the Analysis class to get the wins count of each team so far in the IPL history.

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,22 @@ public class Main {
 
         // Feature 1 - Matches per season
         System.out.println("========================================");
+        System.out.println("Matches per season");
         Map<Integer, Integer> matchesPerSeason = Analysis.matchesPerSeason(matches);
         for (Map.Entry<Integer, Integer> entry: matchesPerSeason.entrySet()) {
             System.out.println("Season " + entry.getKey() + " : " + entry.getValue());
         }
         System.out.println("========================================");
 
+
+        // Feature 2: Total wins by each team
+        System.out.println("========================================");
+        System.out.println("Total wins by each team");
+        Map<String, Integer> teamWins = Analysis.totalMatchesWonByEachTeamTillDate(matches);
+        for (Map.Entry<String, Integer> entry: teamWins.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println("========================================");
 
     }
 }
